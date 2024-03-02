8bb409928eb02582f85df2ee045e62801069c182
Nuzata <lionreo00@icloud.com>
2024-03-02 15:20:42 +0900

    phpを利用

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>Nuzataの趣味サイト</title>
+    </head>
+
+    <body>
+        <!--ヘッダー読み込み-->
+        <?php include "header.html" ?>
+        <main>
+            Nuzataのサイト！
+        </main>
+        <!--フッター読み込み-->
+        <?php include "footer.html" ?>
+    </body>
+</html>
